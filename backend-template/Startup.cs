2f6df6d9533cb9cc4979a646d4e3688536863d57
Rijad <rijad.comor@fsk.unsa.ba>
2024-06-04 22:48:19 +0200

    first commit

---
+++ backend-template/Startup.cs
@@ -0,0 +1,73 @@
+﻿﻿namespace backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
+            //Cors policy za uspostavu komunikacije između API servisa i React projekta
+            services.AddCors();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            //Enable middleware to serve Swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.UseRouting();
+
+            //Nastavak Cors policy-a za React projekat.
+            app.UseCors("SiteCorsPolicy");
+            app.UseCors("CorsPolicy");
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
